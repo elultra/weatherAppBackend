ffdf0a11f9e1d9ce6082166a0083642e9637fee1
elultra <zhuceyong666@outlook.com>
2023-06-15 01:38:41 +1000

    fix hotfix cors

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const swaggerDocs = require("./src/swagger/swagger-config");
 const swaggerUi = require("swagger-ui-express");
 const port = process.env.PORT || 8080;
 
-const corsOptions = {
-    origin: [
-        "https://weatherappnext-ericyui66-outlookcom.vercel.app",
-        "https://weatherappnext-git-main-ericyui66-outlookcom.vercel.app",
-        "http://localhost:3000",
-    ],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: [
+//         "https://weatherappnext-ericyui66-outlookcom.vercel.app",
+//         "https://weatherappnext-git-main-ericyui66-outlookcom.vercel.app",
+//         "http://localhost:3000",
+//     ],
+// };
+app.use(cors());
 app.use("/v1", v1Router);
 app.use(express.json());
 app.use(morgan(process.env.NODE_ENV === "production" ? "common" : "dev"));
