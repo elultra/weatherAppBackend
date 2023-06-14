3d904dbfa8ce4a58acadccaba545b2e22f50beaf
elultra <zhuceyong666@outlook.com>
2023-06-14 19:28:24 +1000

    test: add cors origin

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const swaggerDocs = require("./src/swagger/swagger-config");
 const swaggerUi = require("swagger-ui-express");
 const port = process.env.PORT || 8080;
 
+const corsOptions = {
+    origin: [
+        "https://weatherappnext-ericyui66-outlookcom.vercel.app",
+        "https://weatherappnext-git-main-ericyui66-outlookcom.vercel.app",
+        "http://localhost:3000",
+    ],
+};
 app.use("/v1", v1Router);
 app.use(express.json());
-app.use(
-    cors({
-        origin: "https://weatherappnext-ericyui66-outlookcom.vercel.app/",
-    })
-);
+app.use(cors(corsOptions));
 app.use(morgan(process.env.NODE_ENV === "production" ? "common" : "dev"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
